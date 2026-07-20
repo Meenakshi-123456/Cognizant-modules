eb893288a8b2d7e5429588ad303d2d15ec442ace
Meenakshi-123456 <reddyvennameenakshi@gmail.com>
2026-07-20 12:40:35 +0530

    Update OfficeSpace.js

--- Module-11-React/Exercise-10-JSX-OfficeSpaceRentalApp/src/OfficeSpace.js
+++ Module-11-React/Exercise-10-JSX-OfficeSpaceRentalApp/src/OfficeSpace.js
@@ -8,7 +8,7 @@ function OfficeSpace({ item }) {
     colors.push('textGreen');
   }
 
-  const sr = "https://images.unsplash.com/photo-1497366216548-37526070297c";
+ const sr = "https://images.unsplash.com/photo-1497366216548-37526070297c?w=400&h=300&fit=crop";
   const jsxatt = <img src={sr} width="25%" height="25%" alt="Office Space" />;
 
   return (
